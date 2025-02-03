26516c873852ca3ffbb3e397227cf9de90c50402
Ukashatu40 <121878492+Ukashatu40@users.noreply.github.com>
2025-02-03 19:48:18 +0100

    Update DBConnection.java

--- attendance_management_system/src/main/java/com/ukasha/db/DBConnection.java
+++ attendance_management_system/src/main/java/com/ukasha/db/DBConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/attendance_system";
     private static final String USER = "root";
-    private static final String PASSWORD = "Cand96e442@";
+    private static final String PASSWORD = "password123";
 
     public static Connection getConnection() throws SQLException {
         try {
